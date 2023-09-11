1638581b926eabc10383b8a0b75faa3a3f7fb5c4
Annihilator544 <94665572+Annihilator544@users.noreply.github.com>
2023-09-11 12:37:48 +0530

    Add files via upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,12 @@
 	import Banner from '../components/Banner.svelte';
 	import Hero from '../components/Hero.svelte';
 	import Navbar from '../components/Navbar.svelte';
+	import Letter from '../components/Letter.svelte';
 </script>
 
-<body class= "bg-gradient-to-b from-[#0E2954] via-[#1F6E8C] to-[#2E8A99]">
+<body class= "bg-gradient-to-b from-[#0E2954] via-[#1F6E8C] to-[#2E8A99] ">
 	<Navbar />
 	<Hero />
+	<Letter />
 	<h1>Page</h1>
 </body>
