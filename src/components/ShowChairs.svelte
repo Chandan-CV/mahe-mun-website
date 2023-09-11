4e00c8b6d69c93d7c2cfaedbe9b36b26b2a6b531
Chandan <chandancvp08@gmail.com>
2023-09-11 18:18:53 +0530

    done with committee page

---
+++ src/components/ShowChairs.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Chair } from '$lib/models';
+	import { urlfor } from '$lib/sanityclient';
+
+	export let chair: Chair;
+
+</script>
+
+<div class="p-4 lg:w-1/4 md:w-1/2">
+	<div class="h-full flex flex-col items-center text-center">
+		<img
+			alt="team"
+			class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+			src={urlfor(chair.image).url()}
+		/>
+		<div class="w-full">
+			<h2 class="title-font font-medium text-xl text-gray-900">{chair.name}</h2>
+			<h3 class="text-black mb-3 text-lg">{chair.position}</h3>
+		</div>
+	</div>
+</div>
--- src/lib/sanityclient.ts
+++ src/lib/sanityclient.ts
@@ -1,9 +1,13 @@
-import { createClient } from "@sanity/client";
-
+import { createClient } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
 const client = createClient({
-    projectId: "kaabz9y9",
-    dataset: "production",
-    apiVersion: "2023-09-11", // choose the API version you want
-    useCdn: false,
-    });
+	projectId: 'kaabz9y9',
+	dataset: 'production',
+	apiVersion: '2023-09-11', // choose the API version you want
+	useCdn: false
+});
+const builder = imageUrlBuilder(client);
+export function urlfor(source: string) {
+	return builder.image(source);
+}
 export default client;
