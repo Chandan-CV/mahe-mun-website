20e8a01f82dfbfc890e188a3794228b30e706a81
Annihilator544 <94665572+Annihilator544@users.noreply.github.com>
2023-09-12 12:17:48 +0530

    Add files via upload

---
+++ src/components/Aboutus.svelte
@@ -0,0 +1,14 @@
+<section class="px-4 py-4">
+    <h1 class="flex text-white md:text-6xl text-3xl  mb-7">About Us</h1>
+    <div class="grid md:grid-cols-[60%_40%] grid-cols-1 gap-5">
+    <p class="flex flex-wrap text-white md:text-4xl text-2xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, inventore! Blanditiis omnis recusandae doloremque ipsum facere delectus tempora quis aperiam quia eius, aspernatur nemo eos. Officia explicabo voluptatibus harum quae?
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis ducimus quisquam recusandae eum quia sint provident at, similique quibusdam, alias ad veritatis consequuntur odio et veniam quod unde? Maiores, unde.
+    </p>
+    <!-- svelte-ignore a11y-img-redundant-alt -->
+        <div class="flex justify-center align-middle">
+            <img src="https://source.unsplash.com/random/400x200" class="w-[100%]" alt="random image" />
+    
+        </div>
+    </div>
+</section>
