606133d631fc525cef5c355b74e473a6047f9933
Chandan <chandancvp08@gmail.com>
2023-09-21 12:49:54 +0530

    added new cash prizes

--- src/components/Delegate.svelte
+++ src/components/Delegate.svelte
@@ -1,11 +1,27 @@
-<section class="flex flex-col justify-center items-center bg-[#0E2954] py-12  ">
-    <div class=" grid-cols-1 px-4 py-4 mb-9">
-        <div class="flex justify-center Lemonmilk2 text-center flex-col align-middle items-center">
-            <h1 class="flex justify-center Object text-white md:text-6xl text-3xl text-center  mb-7">TOTAL PRIZE POOL : ₹1.12L</h1>
-            <h1 class="flex justify-center text-white md:text-4xl text-3xl text-center mb-7">Best Delegate: ₹14,000</h1>
-            <h1 class="flex justify-center text-white md:text-4xl text-3xl text-center mb-7">High Comm: ₹7,000</h1>
-            <h1 class="flex justify-center text-white md:text-4xl text-3xl text-center mb-7">Special Mention: ₹4,000</h1>
-            <h1 class="flex justify-center text-white md:text-4xl text-3xl text-center mb-7">Best Delegations: ₹25,000</h1>
-        </div>
-    </div>
+<section class="flex flex-col justify-center items-center bg-[#0E2954] py-12">
+	<div class=" grid-cols-1 px-4 py-4 mb-9">
+		<div class="flex justify-center Lemonmilk2 text-center flex-col align-middle items-center">
+			<h1 class="flex justify-center Object text-white md:text-6xl text-3xl text-center mb-7">
+				TOTAL PRIZE POOL : ₹1.12L
+			</h1>
+			<h1 class="flex justify-center text-white md:text-4xl text-3xl text-center mb-7">
+				Best Delegate: ₹14,000
+			</h1>
+			<h1 class="flex justify-center text-white md:text-4xl text-3xl text-center mb-7">
+				High Comm: ₹7,000
+			</h1>
+			<h1 class="flex justify-center text-white md:text-4xl text-3xl text-center mb-7">
+				Special Mention: ₹4,000 
+			</h1>
+			<h1 class="flex justify-center text-white md:text-4xl text-3xl text-center mb-7">
+				Best Delegations: ₹25,000
+			</h1>
+			<h1 class="flex justify-center text-white md:text-4xl text-3xl text-center mb-7">
+				Best Reporter: ₹6,000
+			</h1>
+			<h1 class="flex justify-center text-white md:text-4xl text-3xl text-center mb-7">
+				Best Photographer: ₹6,000
+			</h1>
+		</div>
+	</div>
 </section>
